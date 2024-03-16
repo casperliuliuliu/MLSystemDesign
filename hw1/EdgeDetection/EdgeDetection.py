7a42d7a79c6ffc31aea71b8b78751e667a28a0a0
Casperliuliuliu <casperliu1118@gmail.com>
2024-03-17 01:16:48 +0800

    complete hw1

--- hw1/EdgeDetection/EdgeDetection.py
+++ hw1/EdgeDetection/EdgeDetection.py
@@ -1,1 +1,26 @@
 #paste your code below
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt 
+
+def increase_contrast(img, alpha=1.5, beta=0):
+    img_float32 = np.float32(img)
+    contrast_img = np.clip(alpha * img_float32 + beta, 0, 255)
+    contrast_img = np.uint8(contrast_img)
+    return contrast_img
+
+def get_cat_outline(img):
+    gray = img[:,:,2]
+    blur = cv2.GaussianBlur(gray, (7, 7), 20)
+    blur = increase_contrast(blur, 2)
+    blur = cv2.GaussianBlur(blur, (7, 7), 0)
+    edges = cv2.Canny(blur, 50, 150)
+    return edges
+
+if __name__ == "__main__":
+    ori_img = cv2.imread('input/cat.jpg')
+    cat_outline = get_cat_outline(ori_img)
+    cv2.imwrite('output/cat.jpg', cat_outline)
+
+    plt.imshow(cat_outline, cmap="gray")
+    plt.show()
