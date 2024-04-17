0fd231becf65c9077b5bb125e15860b853f56905
casperliuliuliu <casperliu1118@gmail.com>
2024-04-18 00:50:46 +0800

    current status

---
+++ hw3/count_file.py
@@ -0,0 +1,13 @@
+import os
+
+def count_files_in_directory(directory):
+
+    file_count = 0
+    
+    for root, dirs, files in os.walk(directory):
+        file_count += len(files)  # Add count of files in the current directory
+        
+    return file_count
+
+folder_path = 'D:\\Casper\\OTHER\\Data\\dpaml_hw3\\dataset\\tzu_photo'  # Replace with your folder path
+print("Number of files in directory:", count_files_in_directory(folder_path))
